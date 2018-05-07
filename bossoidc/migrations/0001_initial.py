9b67086a4e143a0e1ffd5e43e7d6404ec6dd2895
Bramandityo <bramandityo@maksiplus.com>
2018-05-07 17:03:40 +0700

    fixing and add migrations

---
+++ bossoidc/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Keycloak',
+            fields=[
+                ('UID', models.CharField(max_length=37, primary_key=True, serialize=False)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
